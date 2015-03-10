2de5d431a964e051589152d45646896cb1cfb184
Marcus Mo <marcusmo88@gmail.com>
2015-03-10 19:22:47 +0000

    Implemented midpoint displacement in one dimension.

--- app/js/home.js
+++ app/js/home.js
@@ -13,67 +13,83 @@
     x: 0,
     y: svg.height * 0.5,
     width: svg.width,
-    height: 1
+    height: 0.5
   };
 
   // Configuration
   var config = {
-    numIntervals: 100,
+    pm: {
+      min: svg.height * -0.45,
+      max: svg.height *  0.45
+    },
     x: {
       min: 0,
       max: baseline.width,
-      pm: 5 // centred on interval point
     },
     y: {
       min: baseline.y,
       max: baseline.y,
-      pm: 10 // centred on baseline.y
     }
   };
 
-  function randompmone() {
-    // return a random float in [-1, 1)
-    return Math.random() * 2 - 1;
+  // initial line
+  var line = [];
+
+  // return a random float in [-1, 1)
+  function random(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function midpoint(x1, y1, x2, y2) {
+    var x = (x1 + x2) * 0.5;
+    var y = (y1 + y2) * 0.5;
+    return [x, y];
   }
 
   function draw() {
-    // Clear svg
     s.clear();
 
-    // Calculate constrained random interval points
-    var intervalWidth = baseline.width / config.numIntervals;
-    var intervals = [config.x.min, config.y.min];
+    var newLine = [line[0], line[1]];
+    var maxIndex= line.length - 2;
 
-    for(var i = 0; i < config.numIntervals; i++) {
-      // centre points
-      var x = intervalWidth * i;
-      var y = baseline.y;
-
-      // add randomness within tolerances
-      x += config.x.pm * randompmone();
-      y += config.y.pm * randompmone(); 
-
-      intervals.push(x, y);
+    for(var i = 0; i < maxIndex; i+=2) {
+      var mid = midpoint(line[i], line[i+1], line[i+2], line[i+3]);
+      mid[1] += random(config.pm.min, config.pm.max);
+      newLine.push(mid[0], mid[1], line[i+2], line[i+3]);
     }
 
-    intervals.push(config.x.max, config.y.max);
+    line = newLine;
 
     // Draw the line
-    var line = s.polyline(intervals);
-    line.attr({
+    var lineEl = s.polyline(line);
+    lineEl.attr({
       fill: '#fff',
       stroke: '#000',
       strokeWidth: baseline.height
     });
+
+    // Reduce randomness range
+    config.pm.min *= 0.5;
+    config.pm.max *= 0.5;
   }
 
+  function init() {
+    line = [config.x.min, config.y.min, config.x.max, config.y.max];
+    config.pm.min = svg.height * -0.45;
+    config.pm.max = svg.height *  0.45;
+
+    for(var i = 0; i < 10; i++) {
+      draw();
+    }
+  };
+
   // add event listener for click on body
-  d.onclick = draw;
-  d.ontouchstart = draw;
+  d.onclick = init;
+  d.ontouchstart = init;
 
   d.addEventListener('touchmove', function(e) {
     e.preventDefault();
   });
 
-  draw();
+  init();
 })(document, new Snap('#boxes'));
