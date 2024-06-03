ebacef9a7b05ee4ad0fd0741760f852ca0d5a13a
Marcus Mo <marcusmo88@gmail.com>
2024-06-03 20:49:27 +0100

    Refactor map attributions

--- app/js/walks.js
+++ app/js/walks.js
@@ -1,10 +1,17 @@
 (function () {
 
+    var attributions = {
+        "OpenStreetMap contributers": "http://www.openstreetmap.org/copyright",
+        "OpenMapTiles": "http://openmaptiles.org/",
+        "CARTO": "https://carto.com/attributions",
+    }
     var map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}@2x.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributers</a> | &copy; <a href="http://openmaptiles.org/">OpenMapTiles</a> | &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        attribution: Object.entries(attributions)
+            .map(([text, url]) => `&copy; <a href="${url}">${text}</a>`)
+            .join(' | ')
     }).addTo(map);
 
     fetch("2024-06-04-surrey-valleys.json")
