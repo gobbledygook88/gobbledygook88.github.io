67413cf57216e18d1901b4df8d9d7e0cb1a32b72
Marcus Mo <marcusmo88@gmail.com>
2024-06-11 09:53:22 +0100

    Tidy walk js

--- app/js/walks.js
+++ app/js/walks.js
@@ -1,5 +1,8 @@
 (function () {
 
+  const mapEl = document.getElementById("map");
+  if (!mapEl) { return; }
+
   const attributions = {
     "OpenStreetMap contributers": "http://www.openstreetmap.org/copyright",
     "OpenMapTiles": "http://openmaptiles.org/",
@@ -12,29 +15,25 @@
     iconAnchor: [6, 16],
   });
 
-  const mapEl = document.getElementById("map");
+  var map = L.map('map').setView([51.505, -0.09], 13);
 
-  if (mapEl) {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}@2x.png', {
+    maxZoom: 19,
+    attribution: Object.entries(attributions)
+      .map(([text, url]) => `&copy; <a href="${url}">${text}</a>`)
+      .join(' | ')
+  }).addTo(map);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}@2x.png', {
-      maxZoom: 19,
-      attribution: Object.entries(attributions)
-        .map(([text, url]) => `&copy; <a href="${url}">${text}</a>`)
-        .join(' | ')
-    }).addTo(map);
-
-    fetch(mapEl.dataset.mapData)
-      .then(response => response.json())
-      .then(json => {
-        var geojsonLayer = L.geoJSON(json, {
-          "color": "#ff7800",
-          "weight": 3,
-          "opacity": 0.65
-        }).addTo(map);
-        map.fitBounds(geojsonLayer.getBounds());
-      });
-  }
+  fetch(mapEl.dataset.mapData)
+    .then(response => response.json())
+    .then(json => {
+      var geojsonLayer = L.geoJSON(json, {
+        "color": "#ff7800",
+        "weight": 3,
+        "opacity": 0.65
+      }).addTo(map);
+      map.fitBounds(geojsonLayer.getBounds());
+    });
 
   let currentMarker;
   function placeMarker(el) {
