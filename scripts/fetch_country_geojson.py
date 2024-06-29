7aad1de8480d1a05e35a0795f911e5df1e00a60b
Marcus Mo <marcusmo88@gmail.com>
2024-06-29 20:35:37 +0100

    Normalise coordinates for some countries

--- scripts/fetch_country_geojson.py
+++ scripts/fetch_country_geojson.py
@@ -1,8 +1,16 @@
+import math
 import requests
 import geojson
 
 
 SOURCE = "https://raw.githubusercontent.com/georgique/world-geojson/develop"
+
+COUNTRIES_TO_NORMALISE = {
+    ("france", "mainland"),
+    ("italy", "mainland"),
+    ("spain", "mainland"),
+    ("spain", "canary_islands"),
+}
 
 
 def fetch_geojson(country, area):
@@ -21,4 +29,34 @@ def fetch_geojson(country, area):
     for feature in collection.features:
         feature.properties.update({"country": country, "area": area})
 
+    if (country, area) in COUNTRIES_TO_NORMALISE:
+        collection = normalise_coordinates(collection)
+
     return collection
+
+
+def normalise_coordinates(collection):
+    for feature in collection.features:
+        feature.geometry.coordinates = [
+            [
+                normalise_point(latitude, longitude)
+                for latitude, longitude in coordinates
+            ]
+            for coordinates in feature.geometry.coordinates
+        ]
+
+    return collection
+
+
+def normalise_point(latitude, longitude):
+    while math.floor(longitude) < -180:
+        longitude += 360
+    while math.ceil(longitude) > 180:
+        longitude -= 360
+
+    while math.floor(latitude) <= -90:
+        latitude += 180
+    while math.ceil(latitude) >= 90:
+        latitude -= 180
+
+    return [latitude, longitude]
